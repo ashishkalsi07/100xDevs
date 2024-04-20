2c7fc145e8b79577a98dcc3c01bdf4ffcfc2c8f1
Ashish Kalsi <68415917+ashishkalsi07@users.noreply.github.com>
2024-04-20 10:04:32 +0530

    Update countvowels.js

--- Week_1/Medium/countvowels.js
+++ Week_1/Medium/countvowels.js
@@ -1,1 +1,13 @@
-console.log("Uploading Soon")
+function countvowels(str) {
+    const vowelstring = ["a", "e", "i", "o", "u"]
+    const new_str = str.toLowerCase();
+    const uniqvowel=[];
+
+    for (let char of new_str){
+        if(vowelstring.includes(char) && !uniqvowel.includes(char)){
+            uniqvowel.push(char)
+        }
+    }
+    console.log("No. of Vowels in String [Without Duplicates] is "+uniqvowel.length)
+}
+countvowels("AshishKalsi")
