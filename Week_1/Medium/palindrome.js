deb93e573d0b9f36343b10f5a3965152e429812d
ashish kalsi <ashishkalsi07@gmail.com>
2024-04-27 11:38:01 +0530

    Files Added for Week2

--- Week_1/Medium/palindrome.js
+++ Week_1/Medium/palindrome.js
@@ -1,1 +1,9 @@
-console.log("Uploading Soon")
+function isPalindrome(str) {
+    const new_str=str.split("").reduce((acc, char) => char + acc, ""); 
+    return new_str === str
+}
+if (isPalindrome("level")) {
+    console.log("Yes, A Palindrome String")
+} else {
+    console.log("Not a palindrome");
+};
---
+++ Week_2/Easy/async_countter_2.js
@@ -0,0 +1,6 @@
+let counter = 0;
+setTimeout(() => { 
+    counter += 1;
+    console.log("After 4 second") 
+    console.log(counter)
+}, 4000)
