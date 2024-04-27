3338a28553bdada6bbdfc691c8706383cce1e53e
Ashish Kalsi <68415917+ashishkalsi07@users.noreply.github.com>
2024-04-27 12:26:54 +0530

    Delete Easy directory

--- Easy/async_counter_1.js
+++
@@ -1,8 +0,0 @@
-let counter = 0
-let setid = setInterval(()=>{
-    console.log("This is displayed after 1 second")
-    counter += 1;
-    if(counter === 5){
-        clearInterval(setid)
-    }
-},1000)
--- Easy/async_countter_2.js
+++
@@ -1,6 +0,0 @@
-let counter = 0;
-setTimeout(() => { 
-    counter += 1;
-    console.log("After 4 second") 
-    console.log(counter)
-}, 4000)
